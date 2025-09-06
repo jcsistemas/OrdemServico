8c95583450929e71e228a50cae16780513266430
WIN10\servi <jc-sistemas@hotmail.com>
2025-09-05 21:03:22 -0300

    Criação do menu principal, tela sobre, adição de ícones para apresentação de uma interface moderna e amigável, adicionado XP manifest para dar ao sistema uma aparência moderna (este componente XP Manifest fará com que o sistema fique com a aparência de acordo com o windows onde o sistema estiver rodando), criação de opção para salvar imagem de plano de fundo

---
+++ src/uSobre.pas
@@ -0,0 +1,39 @@
+unit uSobre;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, Buttons, ShellAPi;
+
+type
+  TfrmSobre = class(TForm)
+    lbl1: TLabel;
+    lblLinkFatCow: TLabel;
+    btnSair: TSpeedButton;
+    procedure lblLinkFatCowClick(Sender: TObject);
+    procedure btnSairClick(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmSobre: TfrmSobre;
+
+implementation
+
+{$R *.dfm}
+
+procedure TfrmSobre.lblLinkFatCowClick(Sender: TObject);
+begin
+  shellexecute(handle, 'open', PChar(lblLinkFatCow.Caption), NiL, NiL, SW_SHOWNORMAL);
+end;
+
+procedure TfrmSobre.btnSairClick(Sender: TObject);
+begin
+  Close;
+end;
+
+end.
